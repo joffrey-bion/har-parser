564d928dfb75a6237240bbe203698af56500a764
Joffrey Bion <joffrey.bion@gmail.com>
2023-07-17 18:00:17 +0200

    Rework Path extensions
    Resolves:
    https://github.com/joffrey-bion/har-parser/issues/5
    Resolves:
    https://github.com/joffrey-bion/har-parser/issues/6

--- src/jvmMain/kotlin/org/hildan/har/JvmParser.kt
+++ src/jvmMain/kotlin/org/hildan/har/JvmParser.kt
@@ -1,13 +1,26 @@
 package org.hildan.har
 
-import kotlinx.serialization.json.*
 import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 import okio.*
 import java.nio.file.Path
 import kotlin.io.path.*
 
 /**
- * Parses this HAR file into a [Har] object.
+ * Parses this HAR file into a [Har] object, using the provided [json].
+ *
+ * A custom [Json] can be given, such as `Json { ignoreUnknownProperties = false }`, so that potential
+ * new or non-standard properties in the HAR file don't break the parsing.
  */
 @OptIn(ExperimentalSerializationApi::class)
-fun Path.parseHar(): Har = inputStream().use { HarJsonDecoder.decodeFromStream<Har>(it) }
+fun Path.parseHar(json: Json = Json): Har {
+    return inputStream().use { json.decodeFromStream(Har.serializer(), it) }
+}
+
+/**
+ * Writes the given [har] content to this file.
+ */
+@OptIn(ExperimentalSerializationApi::class)
+fun Path.writeHar(har: Har, json: Json = Json) {
+    outputStream().use { json.encodeToStream(Har.serializer(), har, it) }
+}
